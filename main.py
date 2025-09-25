c52a97500f9d7c6484e6cedfdec81612d8bad80c
lemondrop38 <lily.mccormick@ucas-edu.net>
2025-09-25 19:44:38 +0000

    game Co-authored-by: soappp12 <soappp12@users.noreply.github.com> Co-authored-by: unicornmermaid123 <unicornmermaid123@users.noreply.github.com> Co-authored-by: ilovescience-1 <ilovescience-1@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+# LM IG SB VC 6th Build A Game.
 
+
+# Display the game
+
+def welcome(info):
+      return float(input(f"What is your {info}"))#function
+
+
+name = input("What is your name?")#variable
+age = input("How old are you?")#variable
+print(f"Good Morning", {welcome(name)}, "You wake up in a cabin in the middle of the woods, and you dont know where you are so you either...")
+#isabella german the greeting and introduction
